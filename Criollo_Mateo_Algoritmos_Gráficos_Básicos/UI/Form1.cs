f0332340fbac19d21d2a34aac18e70c3aae0a6d0
Teo344 <gabriel2004mat@gmail.com>
2025-06-24 10:29:41 -0500

    Pdfd

---
+++ Criollo_Mateo_Algoritmos_Gráficos_Básicos/UI/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Criollo_Mateo_Algoritmos_Gráficos_Básicos.UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
